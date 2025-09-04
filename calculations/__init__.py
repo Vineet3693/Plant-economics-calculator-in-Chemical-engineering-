ecce4ae12c79021f87982e951a686dddd2abfc52
VINEET YADAV <vineet9949@gmail.com>
2025-09-04 19:55:39 +0530

    Create __init__.py

---
+++ calculations/__init__.py
@@ -0,0 +1,11 @@
+
+---
+
+```python
+# calculations/__init__.py
+from .interest import *
+from .depreciation import *
+from .profitability import *
+from .cost_estimation import *
+from .breakeven import *
+from .replacement import *
